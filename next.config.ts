a9d40b0dc32bbb6b43c8108b30ba23c8480c1ce8
Dchole <johndoe@example.com>
2025-12-30 09:35:40 +0000

    feat: Add redirects for blog section to external URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://dev.to/bugs_bunny",
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
